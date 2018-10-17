5e9fcce7b142d95a1ba75011f177b5ad5e81744b
zhangchengcheng <sweetcczhang@gmail.com>
2018-10-17 23:45:29 +0800

    add influxdb config file

---
+++ conf_influxdb.py
@@ -0,0 +1,10 @@
+#!/usr/local/miniconda2/bin/python
+# _*_ coding:utf-8 _*_
+"""
+# @Project : docker-dashboard
+# @Time    : 2018/10/17 下午11:34
+# @Author  : zhangchengcheng
+# @FileName: conf_influxdb.py
+# @Github  : https://github.com/sweetcczhang
+"""
+
